d999e6097093f81e7b39463635d9918312e3b154
Rubi <mikepaul418@gmail.com>
2026-09-06 00:36:54 +0300

    first commit

---
+++ eclectic_nails/src/components/navbar.tsx
@@ -0,0 +1,12 @@
+
+<nav className="flex justify-between items-center py-6">
+    <div className="bg-[#e09fb3] rounded-full px-5 py-3 text-white text-sm">
+        Eclectic Nails
+    </div>
+
+    <div className="hidden md:flex gap-8 text-white text-sm">
+        <a>Home</a>
+        <a>Services</a>
+        <a>Booking</a>
+    </div>
+</nav>
